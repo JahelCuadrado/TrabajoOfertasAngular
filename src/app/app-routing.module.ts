53446e928836b27f3364f279e84c97cd6dc8fd4b
Jahel <daniel_jahel_cuadrado_tsapp1ob2021@cev.com>
2022-03-12 10:17:20 +0100

    rutas creadas en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { OfertaComponent } from './pages/oferta/oferta.component';
+import { NuevaOfertaComponent } from './pages/nueva-oferta/nueva-oferta.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'nueva-oferta',
+    component: NuevaOfertaComponent
+  },
+  {
+    path: 'oferta/:id',
+    component: OfertaComponent
+  },
+  {
+    path: 'ofertas',
+    component: OfertasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
