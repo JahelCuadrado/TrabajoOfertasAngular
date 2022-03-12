54d74d8213e64b5a4fd1d52717d1fb343dbf1470
Jahel <daniel_jahel_cuadrado_tsapp1ob2021@cev.com>
2022-03-12 12:13:50 +0100

    contedo de oferta service creado

--- src/app/servicios/oferta.service.ts
+++ src/app/servicios/oferta.service.ts
@@ -1,9 +1,17 @@
+import { Oferta } from './../model/oferta';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http"
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obtenerOfertas(): Observable<Array<Oferta>>{
+    return this.http.get<Array<Oferta>>('http://localhost:8080/api/ofertas');
+  }
+
 }
