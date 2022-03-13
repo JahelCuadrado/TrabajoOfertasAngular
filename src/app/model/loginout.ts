318fe2bc87821a350dddd14641ae51d0aac62551
Jahel Cuadrado <jahelcuadradomanchado@hotmail.com>
2022-03-13 08:51:29 +0100

    completos el loginout.ts y el login.service.ts

---
+++ src/app/model/loginout.ts
@@ -0,0 +1,7 @@
+export class Loginout {
+  public id_token:string;
+
+  constructor(id_token:string){
+    this.id_token=id_token;
+  }
+}
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -1,9 +1,48 @@
+import { Loginout } from './../model/loginout';
+import { BehaviorSubject, ConnectableObservable, map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Login } from '../model/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private LoginBehaviourSubject: BehaviorSubject <Loginout | null>;
+  public login: Observable<Loginout | null>;
+
+
+  constructor( private http: HttpClient) {
+
+    let elementoEnNavegador = <string>localStorage?.getItem('login');
+    let elementoSerializado = JSON.parse(elementoEnNavegador);
+    this.LoginBehaviourSubject = new BehaviorSubject<Loginout | null>(elementoSerializado);
+    this.login = this.LoginBehaviourSubject.asObservable();
+
+  }
+
+  hacerLogin(LoginIn:Login):Observable<Loginout>{
+    return this.http.post<Loginout>('http://localhost:8080/api/authenticate', LoginIn)
+    .pipe(map( respueestaBack => {
+
+      this.LoginBehaviourSubject.next(respueestaBack);
+      let value = JSON.stringify(respueestaBack);
+      localStorage.setItem('login', value);
+      console.log('Se guardó del token: ' + value);
+      return respueestaBack;
+
+    }))
+  }
+
+  verUsuarioConectado(): Loginout | null {
+    return this.LoginBehaviourSubject?.value;
+  }
+
+  Logout():void{
+    localStorage.removeItem('login');
+    this.LoginBehaviourSubject.next(null);
+    //redirigir a login
+  }
+
 }
