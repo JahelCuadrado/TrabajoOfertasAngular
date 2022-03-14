c2cc69d9f757146372ccee1eb90a9b4eaedca72e
Jahel <daniel_jahel_cuadrado_tsapp1ob2021@cev.com>
2022-03-14 23:46:19 +0100

    boton de logout creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
+import { Loginout } from './model/loginout';
+import { LoginService } from './servicios/login.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ LoginService ]
 })
 
 export class AppComponent implements OnInit{
   title = 'ejercicio02jahel';
 
-  constructor(private route: Router){}
+  constructor(private route: Router, private loginService: LoginService){}
 
   ngOnInit(): void {
   }
 
   goToLogin() :void{
     this.route.navigate(['/login']);
+  }
+
+  goToLogout() :void{
+    this.loginService.logout();
   }
 }
 
