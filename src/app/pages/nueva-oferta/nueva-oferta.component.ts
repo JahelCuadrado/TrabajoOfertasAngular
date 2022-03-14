046b0b6837077ffc468ac029342cdcf6a5790a2e
Jahel <daniel_jahel_cuadrado_tsapp1ob2021@cev.com>
2022-03-14 22:40:51 +0100

    funcionando la validación del formulario de crear nueva oferta

--- src/app/pages/nueva-oferta/nueva-oferta.component.ts
+++ src/app/pages/nueva-oferta/nueva-oferta.component.ts
@@ -2,62 +2,99 @@ import { LoginService } from './../../servicios/login.service';
 import { OfertaService } from './../../servicios/oferta.service';
 import { Oferta } from 'src/app/model/oferta';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nueva-oferta',
   templateUrl: './nueva-oferta.component.html',
   styleUrls: ['./nueva-oferta.component.css'],
-  providers: [ LoginService ]
+  providers: [LoginService],
 })
 export class NuevaOfertaComponent implements OnInit {
-
   public oferta: Oferta = new Oferta();
-  public formOferta: FormGroup;
-  public formularioValido:boolean=false;
+  public formularioValido: boolean = false;
+  public ofertaModel: Oferta;
+  public validateTitulo: boolean;
+  public validateEmpresa: boolean;
+  public validateDescripcion: boolean;
+  public validateCiudad: boolean;
+  public validateEmail: boolean;
+  public longitudCiudad: number;
 
-  constructor(
-      private ofertaService: OfertaService,
-      private router: Router,
-      private formBuilder: FormBuilder
-  ) {
-      this.formOferta = this.formBuilder.group({
-          titulo: ['', [Validators.required],[Validators.maxLength(100)]],
-          email: ['', [Validators.required],[Validators.maxLength(50)]],
-          salario: ['', [Validators.required]],
-          empresa: ['', [Validators.required],[Validators.maxLength(50)]],
-          descripcion: ['', [Validators.required],[Validators.maxLength(300)]],
-          ciudad: ['', [Validators.required],[Validators.maxLength(50)]],
-      });
-   }
+  constructor(private ofertaService: OfertaService, private router: Router) {
+    this.ofertaModel = new Oferta();
+    this.validateTitulo = false;
+    this.validateEmpresa = false;
+    this.validateDescripcion = false;
+    this.validateCiudad = false;
+    this.validateEmail = false;
+    this.longitudCiudad = 0;
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   public crearOferta(): void {
-    this.formularioValido=false;
-   // if(this.formOferta.valid){
-      this.ofertaService.crearOferta(this.oferta).subscribe(
-          response => {
-              this.goToHome();
-          },
-          error => {
-              console.log('Error ' + JSON.stringify(error));
-          }
-      );
-       // }else{
-        //  this.formularioValido=true;
-       // }
+    this.ofertaService.crearOferta(this.oferta).subscribe(
+      (response) => {
+        this.volver();
+      },
+      (error) => {
+        console.log('Error ' + JSON.stringify(error));
+      }
+    );
   }
 
-  public goToHome(): void{
-      this.router.navigate(['/home']);
+  onSubmit(f: NgForm) {
+    this.validaciones(f);
+
+    this.longitudCiudad = f.value.ciudad.length;
+
+    if (
+      this.validateTitulo === false &&
+      this.validateEmpresa === false &&
+      this.validateDescripcion === false &&
+      this.validateCiudad === false &&
+      this.validateEmail === false
+    ) {
+      this.oferta.titulo = f.value.titulo;
+      this.oferta.descripcion = f.value.descripcion;
+      this.oferta.empresa = f.value.empresa;
+      this.oferta.ciudad = f.value.ciudad;
+      this.oferta.email = f.value.email;
+      this.crearOferta();
+    }
   }
 
-  public volver(): void{
+  public validaciones(f: NgForm): void {
+    if (f.value.titulo.length > 100 || f.value.titulo.length < 5) {
+      this.validateTitulo = true;
+    } else {
+      this.validateTitulo = false;
+    }
+    if (f.value.empresa.length > 50 || f.value.empresa.length < 5) {
+      this.validateEmpresa = true;
+    } else {
+      this.validateEmpresa = false;
+    }
+    if (f.value.descripcion.length > 300 || f.value.descripcion.length < 5) {
+      this.validateDescripcion = true;
+    } else {
+      this.validateDescripcion = false;
+    }
+    if (f.value.ciudad.length > 50 || f.value.ciudad.length < 5) {
+      this.validateCiudad = true;
+    } else {
+      this.validateCiudad = false;
+    }
+    if (f.value.email.length > 50 || f.value.email.length < 5) {
+      this.validateEmail = true;
+    } else {
+      this.validateEmail = false;
+    }
+  }
+
+  public volver(): void {
     this.router.navigate(['/home']);
   }
-
-
-
 }
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -44,4 +44,6 @@ export class LoginService {
     this.route.navigate(['/login']);
   }
 
+  
+
 }
